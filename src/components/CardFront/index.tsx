3200195bbe2c0e58ba64469aa344cae34458daac
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2022-10-10 17:57:20 -0500

    Update card logo

--- src/components/CardFront/index.tsx
+++ src/components/CardFront/index.tsx
@@ -1,14 +1,13 @@
 import './styles.css';
 import cardBackground from '../../assets/images/bg-card-front.png';
-
+import cardLogo from '../../assets/images/card-logo.svg';
 const CardFront = () => {
   return (
     <div className='cardFront'>
       <div className='cardBackground'>
         <img src={cardBackground} alt='front card background' />
       </div>
-      <div className='cardWhiteCircle' />
-      <div className='cardBorderCircle' />
+      <img className='cardLogo' src={cardLogo} alt='' />
       <div className='cardNumber'>9591 6489 6389 1011</div>
       <div className='cardName'>FELICIA LEIRE</div>
       <div className='cardDate'>09/26</div>
