bf91402e3cb3f1a56a2518e31b26e0d27df2a28b
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2022-10-10 21:53:11 -0500

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-import CardBack from './components/CardBack';
-import CardForm from './components/CardForm';
-import CardFront from './components/CardFront';
+import CreditCard from './pages/CreditCard';
 
 function App() {
-  return (
-    <div className='container'>
-      <div className='background' />
-      <div className='cardContainer'>
-        <div className='cardContainerBack'>
-          <CardBack />
-        </div>
-        <div className='cardContainerFront'>
-          <CardFront />
-        </div>
-      </div>
-      <CardForm />
-    </div>
-  );
+  return <CreditCard />;
 }
 
 export default App;
